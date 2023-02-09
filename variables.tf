94d5aa9793da3d9ede267db961fb56d31e4a2bd1
Trieu Tran <trieu.tran@gsa.gov>
2023-02-09 12:06:54 -0500

    Added variables and validation

--- variables.tf
+++ variables.tf
@@ -1,1 +1,28 @@
-# Variable declarations
+# Resource Name Variable declarations
+variable "resource_name" {
+  description = "Name of a cloud resource"
+  type        = string
+  
+  validation {
+    condition = trim() == ""
+    error_message = "Resource's name required"
+  }
+}
+
+# Declare allowed values for Shared Category Tag value
+variable "shared_category_allowed_values" {
+  description = "Allowed value list for Shared Category tag"
+  type = list(string)
+  default = ["TTB", "CDFI", "CUSTOM", "SHARED"]
+}
+
+# Shared Category Tag Variable declarations
+variable "shared_category_tag" {
+  description = "Shared Category tag value for cloud resouces"
+  type = string
+  
+  validation {
+    condition = contain(var.shared_category_allowed_values, var.shared_category_tag)
+    error_message = "Invalid value for Shared Category Tag"
+  }
+}
